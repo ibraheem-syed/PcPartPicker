75570d82eae02391a5d99d6f23165cc21f863764
Noah Corwin <noah.corwin@gmail.com>
2022-11-07 11:36:43 -0800

    cpu selection working!

---
+++ src/SystemConfig.java
@@ -0,0 +1,16 @@
+
+public class SystemConfig {
+	private Processor CPU;
+
+	SystemConfig(){
+		CPU = null;
+	}
+
+	public Processor getCPU() {
+		return CPU;
+	}
+
+	public void setCPU(Processor cPU) {
+		CPU = cPU;
+	}
+}
