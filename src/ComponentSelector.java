f7bb56b5aa093bba4f39a332114509f893165a33
Noah Corwin <noah.corwin@gmail.com>
2022-11-16 16:40:14 -0800

    processor static method to create arraylist

--- src/ComponentSelector.java
+++ src/ComponentSelector.java
@@ -1,29 +1,49 @@
-import java.awt.*;  
-import javax.swing.*;
-import javax.swing.border.LineBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.imageio.ImageIO;
- 
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class ComponentSelector extends JFrame {
+	private static final long serialVersionUID = 1L;
 	final static Integer DIM_MAX = 300;
+	final static Integer X_MAX = 450;
+	final static Integer Y_MAX = 400;
+	
 	private Integer selectedIdx;
 	private ArrayList<? extends Component> compArrList;
 	
 	private String compImgPath;
 	private JButton selectButton = new JButton();
 	private JFrame f = new JFrame();
-	private JList cList = new JList();
-	private JLabel imgLabel;
+	private JList<String> cList = new JList<String>();
+	private static JLabel imgLabel = new JLabel() ;
+	
+	private static void newIcon(String filepath) {
+		BufferedImage compImg = null;
+		Integer x,y,width, height = null;
+	    Double ratio = null;
+	    try { compImg = ImageIO.read(new File(filepath)); }
+	    catch (IOException e) { e.printStackTrace(); }   	    
+	    if(compImg.getHeight() > compImg.getWidth()) {
+	    	ratio = DIM_MAX/Double.valueOf(compImg.getHeight());
+	    }
+	    else { ratio = DIM_MAX/Double.valueOf(compImg.getWidth()); }
+	    width = (int)(ratio * compImg.getWidth());
+	    height = (int)(ratio * compImg.getHeight());
+	    x = (X_MAX - width)/2;
+	    y = (Y_MAX - height)/2;
+ 	    Image compScaled = compImg.getScaledInstance(width,height, Image.SCALE_SMOOTH); 	
+	    imgLabel.setBounds(x,y,width,height);
+    	imgLabel.setIcon(new ImageIcon(compScaled));  
+		System.out.println(filepath);
+	}
 	
 	private class selectButtonListener implements ActionListener {
 	      public void actionPerformed(ActionEvent e)
@@ -44,65 +64,21 @@ public class ComponentSelector extends JFrame {
 		public void valueChanged(ListSelectionEvent ev) {
 	        boolean adjust = ev.getValueIsAdjusting();
 	        if (!adjust) {
-	        	JList list = (JList) ev.getSource();
+	        	JList<?> list = (JList<?>) ev.getSource();
 	        	selectedIdx = list.getSelectedIndex();
-	        	System.out.println(listenComps.get(selectedIdx).modelName);
-	        	System.out.println(listenComps.get(selectedIdx).imagePath);
-	        	String newImg = "images/processors/" + listenComps.get(selectedIdx).imagePath;
-	        	System.out.println(newImg);
-	    		BufferedImage compImg = null;
-	    	    try { compImg = ImageIO.read(new File(newImg)); }
-	    	    catch (IOException e) { e.printStackTrace(); }
-	    	    Integer x,y,width, height = null;
-	    	    Double ratio = null;
-	    	    if(compImg.getHeight() > compImg.getWidth()) {
-	    	    	ratio = DIM_MAX/Double.valueOf(compImg.getHeight());
-	    	    }
-	    	    else { ratio = DIM_MAX/Double.valueOf(compImg.getWidth()); }
-	    	    width = (int)(ratio * compImg.getWidth());
-	    	    height = (int)(ratio * compImg.getHeight());
-	    	    x = (450 - width)/2;
-	    	    y = (400 - height)/2;
-	        
-	    	    Image compScaled = compImg.getScaledInstance(width,height, Image.SCALE_SMOOTH);
-	        	
-	    	    imgLabel.setBounds(x,y,width,height);
-	        	imgLabel.setIcon(new ImageIcon(compScaled));        	
-
+//	        	System.out.println(listenComps.get(selectedIdx).modelName);
+//	        	System.out.println(listenComps.get(selectedIdx).imagePath);
+	        	String newImg = "images/" + compArrList.get(selectedIdx).getImagePath() + "/"+ compArrList.get(selectedIdx).getImageName();
+//	        	System.out.println(newImg);	
+	        	newIcon(newImg);
 	        }
 	        else {       }
-		
 		}
-		
 	}
 	
-	private static ImageIcon newIcon(String filepath) {
 
-		return null;
-	}
 	
-	private static JLabel genImgLabel(String filepath) {
-		JLabel compImgLabel = new JLabel();
-		BufferedImage compImg = null;
-	    try { compImg = ImageIO.read(new File(filepath)); }
-	    catch (IOException e) { e.printStackTrace(); }
-	    Integer x,y,width, height = null;
-	    Double ratio = null;
-	    if(compImg.getHeight() > compImg.getWidth()) {
-	    	ratio = DIM_MAX/Double.valueOf(compImg.getHeight());
-	    }
-	    else { ratio = DIM_MAX/Double.valueOf(compImg.getWidth()); }
-	    width = (int)(ratio * compImg.getWidth());
-	    height = (int)(ratio * compImg.getHeight());
-	    x = (450 - width)/2;
-	    y = (400 - height)/2;
-    
-	    Image compScaled = compImg.getScaledInstance(width,height, Image.SCALE_SMOOTH);
-	    compImgLabel.setIcon(new ImageIcon(compScaled));
-	    compImgLabel.setBounds(x,y,width,height);
-	    
-		return compImgLabel;
-	}
+
 	
 	private static JList<String> genCompList(ArrayList<? extends Component> compArrList) {
 	    
@@ -111,7 +87,8 @@ public class ComponentSelector extends JFrame {
 	    	defaultList.addElement("$" + String.format("%.2f", i.cost)+ 
 	    			" - " + i.manufacturer + ", " + i.modelName);
 	    }
-	    JList<String> compList = new JList<>(defaultList);  
+	    JList<String> compList = new JList<>(defaultList); 
+	    compList.setSelectedIndex(0);
 		return compList;
 	}
 	
@@ -119,8 +96,8 @@ public class ComponentSelector extends JFrame {
 	ComponentSelector(ArrayList<? extends Component> cArrList, SystemConfig myConfig) {  
 		compArrList = cArrList;
 		compArrList.sort((o1, o2) -> o1.cost.compareTo(o2.cost));
-		compImgPath = "images/processors/default.jpg";
-	    	   
+		compImgPath = "images/" + compArrList.get(0).getImagePath() + "/"+ compArrList.get(0).getImageName();
+	    System.out.println(compImgPath);	   
 	    selectButton.setText("Select");
 	    selectButton.setBounds(550,325,100,50);
 	      
@@ -128,7 +105,7 @@ public class ComponentSelector extends JFrame {
 	    cList.setBounds(450,50,300,250);
 	    cList.addListSelectionListener(new selectListener(compArrList));
 	    
-	    imgLabel = genImgLabel(compImgPath);
+	    newIcon(compImgPath);
 	    
         f.add(cList);
 	    f.add(imgLabel);
